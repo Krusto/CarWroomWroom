a2fdaf81288435701dfc7b47f2a86189c14c4469
Krusto Stoyanov <k.stoianov2@gmail.com>
2025-02-15 19:51:59 +0200

    lol

--- UDS/Core/uds/UDS_Handlers/read_data_by_id_handler.cpp
+++ UDS/Core/uds/UDS_Handlers/read_data_by_id_handler.cpp
@@ -4,129 +4,138 @@
 #include <string>
 
 ReadDataByIdentifierHandler::ReadDataByIdentifierHandler() {
-    m_REQ_SID = UDS_READ_DATA_BY_ID_REQUEST_SID;
-    m_RES_SID = UDS_READ_DATA_BY_ID_RESPONSE_SID;
+  m_REQ_SID = UDS_READ_DATA_BY_ID_REQUEST_SID;
+  m_RES_SID = UDS_READ_DATA_BY_ID_RESPONSE_SID;
 }
 
 void ReadDataByIdentifierHandler::MainHandle(Message message) {
-    InitResponseBuffer();
+  InitResponseBuffer();
 
-    if (message.length() != 3) {
-        SendNegativeResponse(NRC::IncorrectMessageLengthOrInvalidFormat);
+  if (message.length() != 3) {
+    SendNegativeResponse(NRC::IncorrectMessageLengthOrInvalidFormat);
+  } else {
+    uint16_t did =
+        (((uint16_t)message[1]) << 8) | ((uint16_t)message[2] & 0xFF);
+    int status = HandleDID(did, message);
+    if (status == 0) {
+      SendPositiveResponse();
     } else {
-        uint16_t did = (((uint16_t) message[1]) << 8) | ((uint16_t) message[2] & 0xFF);
-        int status = HandleDID(did,message);
-        if (status == 0) {
-            SendPositiveResponse();
-        }else{
-            SendNegativeResponse(status);
-        }
+      SendNegativeResponse(status);
     }
+  }
 }
-int ReadDataByIdentifierHandler::HandleDID(uint16_t did, Message& message) {
-    switch (did) {
-        case RDID_VW_Logical_Software_Block_Counter_Of_Programming_Attempts:{
-            for (size_t i = 0; i < 14; i++)
-                AddByteToRespBuffer(0x00);
-            break;
-        } 
-        case RDID_VW_Logical_Software_Block_Lock_Value:{
-            for (size_t i = 0; i < 14; i++)
-                AddByteToRespBuffer(0x00);
-            break;
-        }
-        case RDID_BootloaderTPBlocksize:{
-            AddByteToRespBuffer(0x0F);
-            break;
-        }
-        case RDID_ActiveDiagnosticSession:{
-            AddByteToRespBuffer((uint8_t) SessionData::GetCurrentDiagnosticSession());
-            break;
-        }
-        case RDID_VWSparePartNumber:{
-            for (size_t i = 0; i < 11; i++)
-            {
-                AddByteToRespBuffer('_');
-            }
-            break;
-        }
-        case RDID_VW_logical_block_downgrade_protection_versions:{
-            uint8_t arr[] = {0x01,0x30,0x30,0x30,0x35,0x30,0x30,0x30,0x36,0x30,0x30,0x30,0x35,0x30,0x30,0x30,0x36,0x30,0x30,0x30,0x35,0x30,0x30,0x30,0x35};
-            AddByteArrayToRespBuffer(arr, sizeof(arr)/sizeof(uint8_t));
-            break;
-        }
-        case RDID_VWECUHardwareNumber:{
-            for (size_t i = 0; i < 11; i++)
-            {
-                AddByteToRespBuffer('_');
-            }
-            break;
-        }
-        case RDID_SoftwareVersionNumber: {
-            uint8_t version[4] = {'\0', '\0', '\0', '\0'};
-            uint8_t status = SessionData::ReadBufferFromEEPROM(MEM_RDID_SoftwareVersionNumber_Location,version,MEM_RDID_SoftwareVersionNumber_Size);
-            if(status == 0) {
-                AddByteArrayToRespBuffer(version, MEM_RDID_SoftwareVersionNumber_Size);
-            }else{
-                return NRC::GeneralProgrammingFailure;
-            }
-            break;
-        }
-        case RDID_EngineType: {
-            uint8_t type[13] = {'\0', '\0', '\0', '\0','\0', '\0', '\0', '\0','\0', '\0', '\0', '\0','\0'};
-            uint8_t status = SessionData::ReadBufferFromEEPROM(MEM_RDID_EngineType_Location,type,MEM_RDID_EngineType_Size);
-            if(status == 0) {
-                AddByteArrayToRespBuffer(type, MEM_RDID_EngineType_Size);
-            }else{
-                return 1;
-            }
-            break;
-        }
-        case RDID_ASAM_ODX_File_Identifier:{
-            for (size_t i = 0; i < 25; i++)
-                AddByteToRespBuffer('_');
-            break;
-        }
-        case RDID_VWECUHardwareVersionNumber:{
-            AddByteToRespBuffer('-');
-            AddByteToRespBuffer('-');
-            AddByteToRespBuffer('-');
-        }
-        case RDID_FDSProjectData:{
-            uint8_t arr[] = {0x00,0x00,0x00,0x0A,0x75,0x00,0x00};
-            AddByteArrayToRespBuffer(arr, sizeof(arr)/sizeof(uint8_t));
-            break;
-        }
-        case 0xFFFF:{
-                    uint16_t address = ((uint16_t)message[3]<<8) | ((uint16_t)message[4] & 0xFF);
-                    switch(address){
-                        case RDID_SoftwareVersionNumber:
-                        {
-                            uint8_t version[MEM_RDID_SoftwareVersionNumber_Size] = {'X','_','0','0'};
-                            if (currentSession.WriteBufferToEEPROM(MEM_RDID_SoftwareVersionNumber_Location,version,MEM_RDID_SoftwareVersionNumber_Size) != 0) {
-                                return 1;
-                            }
-                            break;
-                        }
-                        case RDID_EngineType:
-                        {
-                            uint8_t version[MEM_RDID_EngineType_Size] = {'g','a','s','o','l','i','n','e','-','-','-','-','-'};
-                            if (currentSession.WriteBufferToEEPROM(MEM_RDID_EngineType_Location,version,MEM_RDID_EngineType_Size) != 0) {
-                                return 1;
-                            }
-                            break;
-                        }
-                        default:
-                            return 0;
-                    }
-
-                    break;
-                }
-        default:
-            return NRC::RequestOutOfRange;
+int ReadDataByIdentifierHandler::HandleDID(uint16_t did, Message &message) {
+  switch (did) {
+  case RDID_Logical_Software_Block_Counter_Of_Programming_Attempts: {
+    for (size_t i = 0; i < 14; i++)
+      AddByteToRespBuffer(0x00);
+    break;
+  }
+  case RDID_Logical_Software_Block_Lock_Value: {
+    for (size_t i = 0; i < 14; i++)
+      AddByteToRespBuffer(0x00);
+    break;
+  }
+  case RDID_BootloaderTPBlocksize: {
+    AddByteToRespBuffer(0x0F);
+    break;
+  }
+  case RDID_ActiveDiagnosticSession: {
+    AddByteToRespBuffer((uint8_t)SessionData::GetCurrentDiagnosticSession());
+    break;
+  }
+  case RDID_SparePartNumber: {
+    for (size_t i = 0; i < 11; i++) {
+      AddByteToRespBuffer('_');
     }
-    return 0;
+    break;
+  }
+  case RDID_logical_block_downgrade_protection_versions: {
+    uint8_t arr[] = {0x01, 0x30, 0x30, 0x30, 0x35, 0x30, 0x30, 0x30, 0x36,
+                     0x30, 0x30, 0x30, 0x35, 0x30, 0x30, 0x30, 0x36, 0x30,
+                     0x30, 0x30, 0x35, 0x30, 0x30, 0x30, 0x35};
+    AddByteArrayToRespBuffer(arr, sizeof(arr) / sizeof(uint8_t));
+    break;
+  }
+  case RDID_ECUHardwareNumber: {
+    for (size_t i = 0; i < 11; i++) {
+      AddByteToRespBuffer('_');
+    }
+    break;
+  }
+  case RDID_SoftwareVersionNumber: {
+    uint8_t version[4] = {'\0', '\0', '\0', '\0'};
+    uint8_t status = SessionData::ReadBufferFromEEPROM(
+        MEM_RDID_SoftwareVersionNumber_Location, version,
+        MEM_RDID_SoftwareVersionNumber_Size);
+    if (status == 0) {
+      AddByteArrayToRespBuffer(version, MEM_RDID_SoftwareVersionNumber_Size);
+    } else {
+      return NRC::GeneralProgrammingFailure;
+    }
+    break;
+  }
+  case RDID_EngineType: {
+    uint8_t type[13] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0',
+                        '\0', '\0', '\0', '\0', '\0', '\0'};
+    uint8_t status = SessionData::ReadBufferFromEEPROM(
+        MEM_RDID_EngineType_Location, type, MEM_RDID_EngineType_Size);
+    if (status == 0) {
+      AddByteArrayToRespBuffer(type, MEM_RDID_EngineType_Size);
+    } else {
+      return 1;
+    }
+    break;
+  }
+  case RDID_ASAM_ODX_File_Identifier: {
+    for (size_t i = 0; i < 25; i++)
+      AddByteToRespBuffer('_');
+    break;
+  }
+  case RDID_ECUHardwareVersionNumber: {
+    AddByteToRespBuffer('-');
+    AddByteToRespBuffer('-');
+    AddByteToRespBuffer('-');
+  }
+  case RDID_FDSProjectData: {
+    uint8_t arr[] = {0x00, 0x00, 0x00, 0x0A, 0x75, 0x00, 0x00};
+    AddByteArrayToRespBuffer(arr, sizeof(arr) / sizeof(uint8_t));
+    break;
+  }
+  case 0xFFFF: {
+    uint16_t address =
+        ((uint16_t)message[3] << 8) | ((uint16_t)message[4] & 0xFF);
+    switch (address) {
+    case RDID_SoftwareVersionNumber: {
+      uint8_t version[MEM_RDID_SoftwareVersionNumber_Size] = {'X', '_', '0',
+                                                              '0'};
+      if (currentSession.WriteBufferToEEPROM(
+              MEM_RDID_SoftwareVersionNumber_Location, version,
+              MEM_RDID_SoftwareVersionNumber_Size) != 0) {
+        return 1;
+      }
+      break;
+    }
+    case RDID_EngineType: {
+      uint8_t version[MEM_RDID_EngineType_Size] = {
+          'g', 'a', 's', 'o', 'l', 'i', 'n', 'e', '-', '-', '-', '-', '-'};
+      if (currentSession.WriteBufferToEEPROM(MEM_RDID_EngineType_Location,
+                                             version,
+                                             MEM_RDID_EngineType_Size) != 0) {
+        return 1;
+      }
+      break;
+    }
+    default:
+      return 0;
+    }
+
+    break;
+  }
+  default:
+    return NRC::RequestOutOfRange;
+  }
+  return 0;
 }
 
-void ReadDataByIdentifierHandler::PostHandle() {
-}
+void ReadDataByIdentifierHandler::PostHandle() {}
--- UDS/Core/uds/UDS_Handlers/read_data_by_id_handler.hpp
+++ UDS/Core/uds/UDS_Handlers/read_data_by_id_handler.hpp
@@ -4,33 +4,33 @@
 
 #include <string_view>
 
-#define RDID_VW_Logical_Software_Block_Counter_Of_Programming_Attempts 0x0407
-#define RDID_VW_Logical_Software_Block_Lock_Value 0x040F
+#define RDID_Logical_Software_Block_Counter_Of_Programming_Attempts 0x0407
+#define RDID_Logical_Software_Block_Lock_Value 0x040F
 #define RDID_BootloaderTPBlocksize 0x0410
 #define RDID_ActiveDiagnosticSession 0xF186
-#define RDID_VWSparePartNumber 0xF187
-#define RDID_VW_logical_block_downgrade_protection_versions 0x0189
+#define RDID_SparePartNumber 0xF187
+#define RDID_logical_block_downgrade_protection_versions 0x0189
 #define RDID_SoftwareVersionNumber 0xF189
-#define RDID_VWECUHardwareNumber 0xF191
+#define RDID_ECUHardwareNumber 0xF191
 #define RDID_EngineType 0xF197
 #define RDID_ASAM_ODX_File_Identifier 0xF19E
-#define RDID_VWECUHardwareVersionNumber 0xF1A3
+#define RDID_ECUHardwareVersionNumber 0xF1A3
 #define RDID_FDSProjectData 0xF1D5
 
 class ReadDataByIdentifierHandler : public ServiceHandler {
 public:
-    ReadDataByIdentifierHandler();
+  ReadDataByIdentifierHandler();
 
-    virtual void Init() override {}
+  virtual void Init() override {}
 
-    virtual void Destroy() override {};
+  virtual void Destroy() override{};
 
-    virtual void MainHandle(Message message) override;
+  virtual void MainHandle(Message message) override;
 
-    virtual void PostHandle() override;
+  virtual void PostHandle() override;
 
-    static std::string_view GetName() { return "ReadDataByIdentifier handler"; };
+  static std::string_view GetName() { return "ReadDataByIdentifier handler"; };
 
 private:
-    int HandleDID(uint16_t did, Message& message);
+  int HandleDID(uint16_t did, Message &message);
 };
